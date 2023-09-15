716baed6da222210db5eb1147af0bb5e2c04ea6d
zziimm <wlalscjs30@naver.com>
2023-09-15 20:15:51 +0900

    끝

--- js/main.js
+++ js/main.js
@@ -116,3 +116,15 @@ promotionToggleBtnEl.addEventListener('click',function () {
     promotionToggleIconEl.textContent = 'download'
   }
 });
+
+// ScrollMagic 사용
+// 그 외 scrollreveal
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({ // 감시할 장면(Scene) 추가 및 옵션 지정
+    triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+    triggerHook: 0.8 // 화면의 80% 지점에서 보여짐 여부 감시
+  })
+  .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+  .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
+});
